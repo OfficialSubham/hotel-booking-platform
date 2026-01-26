66faecfaef7f7f8dc451b9ddff31444304837256
Subham Mondal <subhamoffi@gmail.com>
2026-01-26 22:12:16 +0530

    Made the login route and have to import jwt then take sign for the token instead of directly importing

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import "dotenv/config";
 
 import express from "express";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
-import { hash } from "bcryptjs";
-import { SignupSchema } from "./schema/UserSchema";
+import { compare, hash } from "bcryptjs";
+import { LoginSchema, SignupSchema } from "./schema/UserSchema";
 import { prisma } from "./db/db";
+import jwt from "jsonwebtoken";
+
+const { sign } = jwt;
 
 const SALT = 10;
 const PORT = process.env.PORT || 3000;
+const JWT_SECRET = process.env.JWT_SECRET || "";
 
 const app = express();
 
@@ -53,6 +57,46 @@ app.post("/api/auth/signup", async (req, res) => {
             error: "EMAIL_ALREADY_EXISTS",
         });
     }
+});
+
+app.post("/api/auth/login", async (req, res) => {
+    const { success, data } = LoginSchema.safeParse(req.body);
+    if (!success)
+        return res.status(400).json({ success, data: null, error: "INVALID_REQUEST" });
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: data.email,
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            role: true,
+            password: true,
+        },
+    });
+    if (!user)
+        return res
+            .status(400)
+            .json({ success: false, data: null, error: "INVALID_CREDENTIALS" });
+    const isPasswordCorrect = await compare(data.password, user.password);
+    if (!isPasswordCorrect)
+        return res
+            .status(401)
+            .json({ success: false, data: null, error: "INVALID_CREDENTIALS" });
+    const token = sign(
+        { userId: user.id, name: user.name, email: user.email, role: user.role },
+        JWT_SECRET,
+    );
+    res.json({
+        success,
+        data: {
+            token,
+            user: { id: user.id, name: user.name, email: user.email, role: user.role },
+        },
+        error: null,
+    });
 });
 
 app.use(globalErrorHandler);
