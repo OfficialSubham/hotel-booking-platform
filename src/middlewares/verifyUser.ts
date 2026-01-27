c956219aca644e8932ef764fee4b1d8c16346e82
Subham Mondal <subhamoffi@gmail.com>
2026-01-27 20:38:23 +0530

    Added unauthorized response for not sending token

--- src/middlewares/verifyUser.ts
+++ src/middlewares/verifyUser.ts
@@ -6,8 +6,12 @@ const JWT_SECRET = process.env.JWT_SECRET || "";
 const { verify } = jwt;
 
 export const verifyUser = (req: Request, res: Response, next: NextFunction) => {
-    const { Authorization } = req.headers as { Authorization: string };
-    const token = Authorization.split(" ")[1];
+    const { authorization } = req.headers as { authorization: string };
+    if (!authorization)
+        return res
+            .status(401)
+            .json({ success: false, data: null, error: "UNAUTHORIZED" });
+    const token = authorization.split(" ")[1];
     try {
         const data = verify(token, JWT_SECRET) as JwtData;
         req.userId = data.userId;
